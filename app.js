139c774848864758c473891e46a7534a57c60849
LeandroCostaFullDev <leandro181993@gmail.com>
2025-01-28 21:52:11 -0300

    updade28.01

--- app.js
+++ app.js
@@ -164,3 +164,319 @@
 
 // // numeros.pop()
 // // console.log(numeros);
+
+// ------------------------------------------------------
+
+// Dado um array de nomes, use map para criar um novo array com todos os nomes em letras maiusculas
+
+// const names = ["ana", "maria", "joão"]
+
+// const resultado = names.map(name => name.toUpperCase())
+
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+
+// Dado um array de objetos, use map para criar um novo array contendo apenas os valores de uma propriedade específica.
+
+// const users = [
+//     { id: 1, name: "Alice"},
+//     { id: 2, name: "Bob"},
+//     { id: 3, name: "Charlie"},
+// ];
+
+// let resultado = users.map(user => `${user.name}`)
+
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// Dado um array de strings, use filter para criar um novo array contendo apenas as strings que possuem mais de 5 caracteres.
+
+// const words = ["javascript", "map", "filter", "react", "vue"];
+
+// let resultado = words.filter(word => word.length > 5)
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// Dado um array que contém valores nulos ou indefinidos, use filter para criar um novo array sem esses valores.
+
+// const mixedArray = [1, null, 2, null, 3, 4];
+
+// const resultado = mixedArray.filter(newArray => newArray !== null)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de números, use filter para selecionar os números pares e, em seguida, map para dobrá-los.
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const resultado = numbers.filter(number => number%2 ===0).map(number => number *2)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de nomes, use map para transformá-los em maiúsculas e filter para selecionar apenas os que têm menos de 5 caracteres.
+
+// const names = ["Ana", "Maria", "João", "Eva", "Carlos"];
+
+// const resultado = names.filter(name => name.length<5).map(name => name.toUpperCase())
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de produtos, use filter para retornar apenas os produtos com preço acima de 700.
+
+// const products = [
+//   { name: "Laptop", price: 1000 },
+//   { name: "Phone", price: 500 },
+//   { name: "Tablet", price: 750 },
+// ];
+
+// const resultado = products.filter(product => product.price>700)
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// PRATICAIVOOOOOOOOOOSS
+
+// Crie um array de números e utilize o método map para retornar um novo array com todos os números dobrados.
+
+// const numbers = [1, 2, 3, 4, 5];
+// const resultado = numbers.map(number => number*2)
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// Dado um array de nomes, use map para criar um novo array com todos os nomes em letras maiúsculas.
+
+// const names = ["ana", "maria", "joão"];
+
+// const resultado = names.map(name => name.toUpperCase())
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+
+// Dado um array de objetos, use map para criar um novo array contendo apenas os valores de uma propriedade específica.
+
+// const users = [
+//   { id: 1, name: "Alice" },
+//   { id: 2, name: "Bob" },
+//   { id: 3, name: "Charlie" },
+// ];
+
+// const resultado = users.map(user => `${user.name}`)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de números, use filter para criar um novo array contendo apenas os números pares.
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const resultado = numbers.filter(number => number%2===0)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de strings, use filter para criar um novo array contendo apenas as strings que possuem mais de 5 caracteres.
+
+// const words = ["javascript", "map", "filter", "react", "vue"];
+
+// const resultado = words.filter(word => word.length >5 )
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de objetos de usuários, filtre os usuários com idade maior ou igual a 18 anos.
+
+// const users = [
+//   { name: "Alice", age: 17 },
+//   { name: "Bob", age: 20 },
+//   { name: "Charlie", age: 16 },
+// ];
+
+// const idade = users.filter(user => user.age >= 18)
+// console.log(idade);
+
+// ------------------------------------------------------
+
+// Dado um array que contém valores nulos ou indefinidos, use filter para criar um novo array sem esses valores.
+
+// const mixedArray = [1, null, 2, null, 3, 4];
+
+// const resultado = mixedArray.filter(array => array !== null)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de números, use filter para selecionar os números pares e, em seguida, map para dobrá-los.
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const resultado = numbers.filter(number => number%2===0).map(number => number*2)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de nomes, use map para transformá-los em maiúsculas e filter para selecionar apenas os que têm menos de 5 caracteres.
+
+// const names = ["Ana", "Maria", "João", "Eva", "Carlos"];
+
+// const resultado = names.map(name => name.toUpperCase()).filter(name => name.length <5)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de usuários, use filter para selecionar apenas os usuários ativos e map para retornar um array com os nomes desses usuários.
+
+// const users = [
+//   { name: "Alice", active: true },
+//   { name: "Bob", active: false },
+//   { name: "Charlie", active: true },
+// ];
+
+// const resultado = users.filter(user => user.active === true).map(user => user.name )
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de strings, use map para criar tags <p> ao redor de cada string.
+
+// const texts = ["Hello", "World", "JavaScript"];
+
+// const resultado = texts.map(text => `<p>${text}</p>`)
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// Dado um array de números, use filter para criar um novo array contendo apenas os números que são divisíveis por 3.
+
+// const numbers = [1, 3, 4, 6, 9, 10, 12, 15];
+
+// const resultado = numbers.filter(number => number %3===0)
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// Dado um array de números, use filter para selecionar os pares e, em seguida, map para transformá-los em números ímpares somando 1.
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const resultado = numbers.filter(number => number %2===0).map(number => number +1)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Use map para formatar um array de usuários para que cada elemento seja uma string no formato: 
+
+// const users = [
+//   { name: "Alice", age: 25 },
+//   { name: "Bob", age: 30 },
+//   { name: "Charlie", age: 35 },
+// ];
+
+
+// ------------------------------------------------------
+
+// Dado um array de produtos, use filter para retornar apenas os produtos com preço acima de 700.
+
+// const products = [
+//   { name: "Laptop", price: 1000 },
+//   { name: "Phone", price: 500 },
+//   { name: "Tablet", price: 750 },
+// ];
+
+// const resultado = products.filter(prod => prod.price > 700)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de pedidos, use filter para retornar apenas os pedidos com quantidade maior que 1.
+
+// const orders = [
+//   { product: "Laptop", quantity: 1 },
+//   { product: "Phone", quantity: 3 },
+//   { product: "Tablet", quantity: 2 },
+// ];
+
+// const resultado = orders.filter(ord => ord.quantity > 1)
+// console.log(resultado);
+
+// ------------------------------------------------------
+
+// Dado um array de objetos representando funcionários, filtre apenas aqueles cujo status seja "ativo".
+
+// const employees = [
+//   { name: "Alice", status: "ativo" },
+//   { name: "Bob", status: "inativo" },
+//   { name: "Charlie", status: "ativo" },
+// ];
+
+// const resultado = employees.filter(stat => stat.status === "ativo")
+// console.log(resultado);
+
+
+// ------------------------------------------------------
+
+// Dado um array de alunos com suas respectivas notas, filtre apenas os alunos aprovados (nota >= 7).
+
+// const students = [
+//   { name: "Alice", grade: 6 },
+//   { name: "Bob", grade: 8 },
+//   { name: "Charlie", grade: 7 },
+//   { name: "David", grade: 5 },
+// ];
+
+// const nota = students.filter(not => not.grade >= 7)
+// console.log(nota);
+
+
+// ------------------------------------------------------
+
+// Use filter e map para obter os nomes dos alunos com idade maiores  ou igual que 25
+
+// const students = [
+//   { name: 'Alice', age: 16 },
+//   { name: 'Bob', age: 18 },
+//   { name: 'Charlie', age: 27 },
+//   { name: 'David', age: 25 },
+// ];
+
+// const idade = students.filter(stu => stu.age >=25).map(stu => stu.name)
+// console.log(idade);
+
+
+// --------------------------------------------------------------------------------------------------
+
+// Crie uma função chamada multiplicar que recebe dois parâmetros: uma lista de números e um número pelo qual todos os elementos da lista devem ser multiplicados. Em seguida, use map para aplicar essa função à lista de números.
+
+// Crie uma função chamada filtrarPares que recebe uma lista de números e filtra apenas os números pares dessa lista, utilizando filter.
+
+
+// Crie uma função chamada processarLista que primeiro usa filter para filtrar números ímpares e depois usa map para multiplicar os números restantes por 10.
+
+// Crie uma função chamada transformarNumeros que recebe uma lista de números e um parâmetro operacao. Se o parâmetro for "quadrado", a função deve retornar uma lista com os quadrados dos números; se for "raiz", deve retornar a raiz quadrada de cada número. Utilize map para realizar a transformação
+
+// Crie uma função chamada filtrarPorIdade que recebe uma lista de objetos representando pessoas (com as propriedades nome e idade) e um parâmetro idadeMinima. A função deve retornar apenas as pessoas cuja idade seja maior ou igual à idadeMinima, utilizando filter.
+
+// Crie uma função chamada filtrarMultiplicar que recebe uma lista de números e um número multiplicador. A função deve primeiro filtrar todos os números maiores que 10 e, em seguida, multiplicar esses números pelo valor do multiplicador.
+
+
+// Crie uma função chamada classificarIdadePorCategoria que recebe uma lista de idades e classifica cada idade em uma categoria. As categorias são:
+
+// "Criança" para idades entre 0 e 12.
+// "Adolescente" para idades entre 13 e 18.
+// "Adulto" para idades entre 19 e 59.
+// "Idoso" para idades a partir de 60.
